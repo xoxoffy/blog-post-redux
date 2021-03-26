18f65f1b6c723e226cfe08abc14b530f42bc3f90
Dominik <dominik.wozniewicz@gmail.com>
2021-03-26 05:48:26 +0100

    Memoized the request to the API to fetch the users just one time

--- src/actions/ActionCreators.js
+++ src/actions/ActionCreators.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 // Defining a function that returns a function (redux thunk makes it possible
@@ -8,8 +9,12 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
+export const fetchUser = (id) => (dispatch) => {
+  _fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: response.data });
-};
+});
--- src/components/UserHeader.jsx
+++ src/components/UserHeader.jsx
@@ -2,12 +2,21 @@ import { useEffect } from "react";
 import { connect } from "react-redux";
 import { fetchUser } from "../actions/ActionCreators";
 
-const UserHeader = ({ fetchUser, userId }) => {
+const UserHeader = ({ fetchUser, userId, user }) => {
   useEffect(() => {
     fetchUser(userId);
   }, []);
 
-  return <div>User Header</div>;
+  if (!user) {
+    return null;
+  }
+
+  return <div className="header">{user.name}</div>;
 };
 
-export default connect(null, { fetchUser })(UserHeader);
+const mapStateToProps = (state, ownProps) => {
+  // ownProps is a reference to the props in the component so we can refer to userId
+  return { user: state.users.find((user) => user.id === ownProps.userId) };
+};
+
+export default connect(mapStateToProps, { fetchUser })(UserHeader);
