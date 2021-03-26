790c64bd7fc476a23889aa7432f0dc5db476c5a2
Dominik <dominik.wozniewicz@gmail.com>
2021-03-26 06:27:43 +0100

    Added comments

--- src/actions/ActionCreators.js
+++ src/actions/ActionCreators.js
@@ -1,18 +1,21 @@
 import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
+// Action creator inside an action creator
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   // the await keyword here makes sure that the API request from fetchPosts
   // will be completed before we move on and do anything else
   await dispatch(fetchPosts());
 
+  // getState.posts = list of all posts
   // Find the unique user ID using lodash
   const userIds = _.uniq(_.map(getState().posts, "userId"));
   userIds.forEach((id) => dispatch(fetchUser(id)));
 };
 
 // Defining a function that returns a function (redux thunk makes it possible
-// to return actions as functions, not objects necessary)
+// to return actions as functions, not objects necessary) with the keywords dispatch
+// and optionally getState
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
 
